abd0bd14f74760f4d274390c3945e53e7ad408a2
Dia Dana <danad@whitman.edu>
2025-09-29 13:24:25 -0700

    dotproduct of the two vectors

--- dotproduct.c
+++ dotproduct.c
@@ -1,10 +1,18 @@
+#include <stdio.h>
 
 int main() {
     int dotProduct = 0;
     int firstVector[3], secondVector[3];
 
-    printf("Enter 3 components of teh first vector");
+    printf("Enter 3 components of teh first vector: ");
     scanf("%d %d %d", &firstVector[0], &firstVector[1], &firstVector[2]);
-    
 
+    printf("Enter 3 components of the second vector: ");
+    scanf("%d %d %d", &secondVector[0], &secondVector[1], &secondVector[2]);
+    
+    for (int i =0; i < 3; i++) {
+        dotProduct += firstVector[i] * secondVector[i];
+    }
+
+    printf("%d\n", dotProduct);
 }
