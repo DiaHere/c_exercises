92df58e7aa97c62f43007d155cc0e4bf5ab4cc8a
Dia Dana <danad@whitman.edu>
2025-09-29 13:13:20 -0700

    add main

---
+++ dotproduct.c
@@ -0,0 +1,10 @@
+
+int main() {
+    int dotProduct = 0;
+    int firstVector[3], secondVector[3];
+
+    printf("Enter 3 components of teh first vector");
+    scanf("%d %d %d", &firstVector[0], &firstVector[1], &firstVector[2]);
+    
+
+}
